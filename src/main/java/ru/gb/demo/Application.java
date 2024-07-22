d3198aaa967c037098218a9f460f131ae7b880c8
Anton <B_atonyo_S@bk.ru>
2024-07-22 05:31:32 +0300

    Completed TASK ONE

--- src/main/java/ru/gb/demo/Application.java
+++ src/main/java/ru/gb/demo/Application.java
@@ -4,19 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import ru.gb.demo.aspect.NameLoggerAspect;
 import ru.gb.demo.service.ReaderService;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class Application {
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-		ReaderService readerService = context.getBean(ReaderService.class);
-		NameLoggerAspect nameLoggerAspect = context.getBean(NameLoggerAspect.class);
-		System.out.println(nameLoggerAspect.getClass());
-		System.out.println(readerService.ReaderInfo(1L));
-
+	public static void main(String[] args) { SpringApplication.run(Application.class, args);
 	}
 
 }
